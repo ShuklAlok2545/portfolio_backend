addbeb50595cff2e71a2627a42d25978cf2f6e4f
Alok kumar <166221534+ShuklAlok2545@users.noreply.github.com>
2025-05-19 12:38:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use(cors({
 
 app.use(express.json());
 
-connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+connect(MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));
 
